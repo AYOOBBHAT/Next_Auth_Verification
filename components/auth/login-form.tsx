eb39bcb3a9ee1ad1e41084b673ccfb67618f7cb4
Ayoob Bhat <ayoob324005@gmail.com>
2024-01-18 20:47:57 +0530

    added Card Wrapper

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,23 @@
+
+import { CardWrapper } from "./card-wrapper";
+
+export  const LoginForm=()=>{
+
+
+    return (
+    <CardWrapper
+    headerLabel="welcome back"
+    backButtonLabel="Don't have an account"
+    backButtonHref="/aut/register"
+    showSocial> 
+    
+     loginForm
+    </CardWrapper>
+    
+   
+    
+    
+    )
+}
+
+export default LoginForm;
